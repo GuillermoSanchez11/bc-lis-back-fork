e97a20cf05cdc06a85a3b4b81819ac9eab183296
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2024-09-06 09:02:54 -0500

    feat(repository): add repositories for JWT

---
+++ src/main/java/com/bclis/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.bclis.repository;
+
+import com.bclis.model.entity.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+}
