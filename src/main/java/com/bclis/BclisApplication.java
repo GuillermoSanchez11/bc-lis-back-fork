3ca26f7e31a0bbdde46f535edc42c4e19d6b710a
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2024-09-05 22:05:54 -0500

    first commit

---
+++ src/main/java/com/bclis/BclisApplication.java
@@ -0,0 +1,13 @@
+package com.bclis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BclisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BclisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bclis/BclisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bclis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BclisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
