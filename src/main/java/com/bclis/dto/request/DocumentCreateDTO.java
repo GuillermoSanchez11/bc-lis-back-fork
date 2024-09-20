523e573966b53b774dc5b4d414491b8bfcc16821
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-09-20 12:03:03 -0500

    feat: add document DTO's

--- src/main/java/com/bclis/dto/request/DocumentCreateDTO.java
+++ src/main/java/com/bclis/dto/request/DocumentCreateDTO.java
@@ -1,2 +1,20 @@
-package com.bclis.dto.request;public class DocumentCreateDTO {
+package com.bclis.dto;
+
+import com.bclis.persistence.entity.DocumentEntity.DocumentState;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DocumentCreateDTO {
+
+    private Long userId;
+    private String name;
+    private byte[] content;
+    private DocumentState state;
+    private Long typeId;
 }
--- src/main/java/com/bclis/dto/response/DocumentResponseDTO.java
+++ src/main/java/com/bclis/dto/response/DocumentResponseDTO.java
@@ -1,2 +1,25 @@
-package com.bclis.dto.response;public class DocumentResponseDTO {
+package com.bclis.dto;
+
+import com.bclis.persistence.entity.DocumentEntity.DocumentState;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DocumentResponseDTO {
+
+    private Long id;
+    private Long userId;
+    private String userName;
+    private String name;
+    private byte[] content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private DocumentState state;
+    private Long typeId;
+    private String typeName;
 }
