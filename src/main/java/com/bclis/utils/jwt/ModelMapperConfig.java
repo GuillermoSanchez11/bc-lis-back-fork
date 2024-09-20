833c3398dc36f4065b94a9156ec100206ecb515b
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-09-20 12:05:14 -0500

    feat: add modelMapper config

--- src/main/java/com/bclis/utils/jwt/ModelMapperConfig.java
+++ src/main/java/com/bclis/utils/jwt/ModelMapperConfig.java
@@ -1,2 +1,14 @@
-package com.bclis.utils.jwt;public class ModelMapperConfig {
+package com.bclis.utils.jwt;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
